23c105a59fec7008b56478b1a8b4d895e92167de
Liu Lan <liulan@cmss.chinamobile.com>
2023-07-28 10:38:35 +0800

    chore: add common.properties

--- run.sh
+++ run.sh
@@ -1,61 +1,70 @@
 #!/bin/bash
 
-dynamodbtablename=ycsb
-dynamodbprimarykeytype=HASH_AND_RANGE
-dynamodbprimarykey=_key
-dynamodbhashkey=hashkey
-dynamodbhashkeyvalue=hash
-dynamodbrcunits=10
-dynamodbwcunits=10
-dynamodbensurecleantable=false
-dynamodbregion=us-east-1
-dynamodbconsistentreads=false
-dynamodbdeleteafterrunstage=false
-dynamodbendpoint=http://100.71.8.128:80
 
-recordcount=50000000
-operationcount=50000000
-workload=core
-fieldcount=5
-fieldlength=100
+./bin/go-ycsb load dynamodb -P dynamodb.properties -P workloads/workloada --threads 8 & >> result
+./bin/go-ycsb load dynamodb -P dynamodb.properties -P workloads/workloada --threads 8 & >> result
+./bin/go-ycsb load dynamodb -P dynamodb.properties -P workloads/workloada --threads 8 & >> result
+./bin/go-ycsb load dynamodb -P dynamodb.properties -P workloads/workloada --threads 8 & >> result
+./bin/go-ycsb load dynamodb -P dynamodb.properties -P workloads/workloada --threads 8 & >> result
 
-readallfields=true
+tailf result
 
-readproportion=1
-updateproportion=0
-scanproportion=0
-insertproportion=0
-
-requestdistribution=uniform
-threads=16
-mode=load
-
-image="registry.paas/eos-toolchains/go-ycsb:fdb-7.1.33"
-
-docker run $image $mode dynamodb --threads=$threads \
-    -p recordcount=$recordcount \
-    -p operationcount=$operationcount \
-    -p workload=core \
-    -p fieldcount=$fieldcount \
-    -p fieldlength=$fieldlength \
-    -p readallfields=$readallfields \
-    -p readproportion=$readproportion \
-    -p updateproportion=$updateproportion \
-    -p scanproportion=$scanproportion \
-    -p insertproportion=$insertproportion \
-    -p requestdistribution=$requestdistribution \
-    -p dynamodb.tablename=$dynamodbtablename \
-    -p dynamodb.ensure.clean.table=$dynamodbensurecleantable \
-    -p dynamodb.primarykey.type=$dynamodbprimarykeytype \
-    -p dynamodb.primarykey=$dynamodbprimarykey \
-    -p dynamodb.hashkey=$dynamodbhashkey \
-    -p dynamodb.hashkey.value=$dynamodbhashkeyvalue \
-    -p dynamodb.rc.units=$dynamodbrcunits \
-    -p dynamodb.wc.units=$dynamodbwcunits \
-    -p dynamodb.region=$dynamodbregion \
-    -p dynamodb.consistent.reads=$dynamodbconsistentreads \
-    -p dynamodb.delete.after.run.stage=$dynamodbdeleteafterrunstage \
-    -p dynamodb.endpoint=$dynamodbendpoint
+# dynamodbtablename=ycsb
+# dynamodbprimarykeytype=HASH_AND_RANGE
+# dynamodbprimarykey=_key
+# dynamodbhashkey=hashkey
+# dynamodbhashkeyvalue=hash
+# dynamodbrcunits=10
+# dynamodbwcunits=10
+# dynamodbensurecleantable=false
+# dynamodbregion=us-east-1
+# dynamodbconsistentreads=false
+# dynamodbdeleteafterrunstage=false
+# dynamodbendpoint=http://100.71.8.128:80
+# 
+# recordcount=50000000
+# operationcount=50000000
+# workload=core
+# fieldcount=5
+# fieldlength=100
+# 
+# readallfields=true
+# 
+# readproportion=1
+# updateproportion=0
+# scanproportion=0
+# insertproportion=0
+# 
+# requestdistribution=uniform
+# threads=16
+# mode=load
+# 
+# image="registry.paas/eos-toolchains/go-ycsb:fdb-7.1.33"
+# 
+# docker run $image $mode dynamodb --threads=$threads \
+#     -p recordcount=$recordcount \
+#     -p operationcount=$operationcount \
+#     -p workload=core \
+#     -p fieldcount=$fieldcount \
+#     -p fieldlength=$fieldlength \
+#     -p readallfields=$readallfields \
+#     -p readproportion=$readproportion \
+#     -p updateproportion=$updateproportion \
+#     -p scanproportion=$scanproportion \
+#     -p insertproportion=$insertproportion \
+#     -p requestdistribution=$requestdistribution \
+#     -p dynamodb.tablename=$dynamodbtablename \
+#     -p dynamodb.ensure.clean.table=$dynamodbensurecleantable \
+#     -p dynamodb.primarykey.type=$dynamodbprimarykeytype \
+#     -p dynamodb.primarykey=$dynamodbprimarykey \
+#     -p dynamodb.hashkey=$dynamodbhashkey \
+#     -p dynamodb.hashkey.value=$dynamodbhashkeyvalue \
+#     -p dynamodb.rc.units=$dynamodbrcunits \
+#     -p dynamodb.wc.units=$dynamodbwcunits \
+#     -p dynamodb.region=$dynamodbregion \
+#     -p dynamodb.consistent.reads=$dynamodbconsistentreads \
+#     -p dynamodb.delete.after.run.stage=$dynamodbdeleteafterrunstage \
+#     -p dynamodb.endpoint=$dynamodbendpoint
 
 # fdbcluster=/etc/foundationdb/fdb.cluster
 # fdbversion=710
